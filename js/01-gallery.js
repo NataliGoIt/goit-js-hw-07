13409af577da5c12d01ec3163cf17c650ebb95c7
NataliGoIt <nataliakal4enko@gmail.com>
2021-10-24 16:31:26 +0300

    разметка галереи

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const galleryCreate = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image"
+   src="${preview}"
+   data-source="${original}"
+   alt="${description}" /> </a>
+</div>`;
+  })
+  .join('');
+
+galleryEl.addEventListener('click', event => {
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `<div class="modal">
+    <img src="${event.target.dataset.source}" width="800" height="600">
+    <a class="close-lightbox">Close</a>
+    </div>`,
+    {
+      onShow: instance => {
+        instance
+          .element('.modal')
+          .querySelector('.close-lightbox')
+          .addEventListener('click', instance.close, { once: true });
+      },
+    },
+  );
+  instance.show();
+});
+galleryEl.insertAdjacentHTML('afterbegin', galleryCreate);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,18 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
+const galleryCreate = galleryItems.map(({ preview, original, description }) => {
+  return ` <a class="gallery__item" href="${original}"> <img class="gallery__image"
+src="${preview}"
+alt="${description}" />
+</a>`;})
+.join('');
 
-console.log(galleryItems);
+galleryEl.insertAdjacentHTML('afterbegin', galleryCreate);
+
+const gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'botton',
+});
+
