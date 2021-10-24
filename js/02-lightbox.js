54925654fbed4e2b1a7d09fda9d7927a912b3d2a
NataliGoIt <nataliakal4enko@gmail.com>
2021-10-24 16:40:29 +0300

    bag

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,18 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryEl = document.querySelector('.gallery');
-const galleryCreate = galleryItems.map(({ preview, original, description }) => {
-  return ` <a class="gallery__item" href="${original}"> <img class="gallery__image"
+const galleryCreate = galleryItems
+  .map(({ preview, original, description }) => {
+    return ` <a class="gallery__item" href="${original}"> <img class="gallery__image"
 src="${preview}"
 alt="${description}" />
-</a>`;})
-.join('');
+</a>`;
+  })
+  .join('');
 
 galleryEl.insertAdjacentHTML('afterbegin', galleryCreate);
 
 let gallery = new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-    captionPosition: 'botton',
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'botton',
 });
-
